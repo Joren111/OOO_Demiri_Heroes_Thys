8584e63154a693f397fb63c50522e136120b3935
Aurel Demiri <demiriaurel@gmail.com>
2018-11-27 16:16:43 +0100

    Edited method signature of get db methods

--- 1718_selfAssess_opgave/src/domain/db/BadDb.java
+++ 1718_selfAssess_opgave/src/domain/db/BadDb.java
@@ -7,6 +7,7 @@ import javafx.collections.ObservableList;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,39 +23,55 @@ public class BadDb {
         return instance;
     }
 
-    public ObservableList<Category> getCategoryList() throws Exception {
+    public ObservableList<Category> getCategoryList() {
         if (categoryList == null) {
             List<Category> list = new ArrayList<>();
-            BufferedReader br = new BufferedReader(new FileReader("testdatabase\\groep.txt"));
-            String s;
-            while ((s = br.readLine()) != null) {
-                Category category = new Category();
-                String data[] = s.split(",");
-                category.setTitle(data[0]);
-                category.setDescription(data[1]);
-                list.add(category);
+            BufferedReader br;
+            try {
+                br = new BufferedReader(new FileReader("testdatabase\\groep.txt"));
+                String s;
+                while ((s = br.readLine()) != null) {
+                    Category category = new Category();
+                    String data[] = s.split(",");
+                    category.setTitle(data[0]);
+                    category.setDescription(data[1]);
+
+                    // size of 3 means that category has a subcategory
+                    if (data.length == 3)
+                        category.setSubCategory(list.stream().filter(x -> x.getTitle().equals(data[2])).findFirst().get());
+
+                    list.add(category);
+                }
+
+                categoryList = FXCollections.observableArrayList(list);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-            categoryList = FXCollections.observableArrayList(list);
         }
 
         return this.categoryList;
     }
 
-    public ObservableList<Question> getQuestionList() throws Exception {
+    public ObservableList<Question> getQuestionList() {
         if (questionList == null) {
             List<Question> list = new ArrayList<>();
-            BufferedReader br = new BufferedReader(new FileReader("testdatabase\\vraag.txt"));
-            String s;
-            while ((s = br.readLine()) != null) {
-                Question question = new Question();
-                String data[] = s.split(",");
-                question.setQuestion(data[0]);
-                question.setCategory(data[1]);
-                list.add(question);
-            }
+            BufferedReader br;
+            try {
+                br = new BufferedReader(new FileReader("testdatabase\\vraag.txt"));
+                String s;
+                while ((s = br.readLine()) != null) {
+                    Question question = new Question();
+                    String data[] = s.split(",");
+                    question.setQuestion(data[0]);
+                    question.setCategory(data[1]);
+                    list.add(question);
+                }
 
-            questionList = FXCollections.observableArrayList(list);
+                questionList = FXCollections.observableArrayList(list);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         return questionList;
