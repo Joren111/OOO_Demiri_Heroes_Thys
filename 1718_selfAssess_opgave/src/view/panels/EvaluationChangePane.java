e4a8cf18a3a6e167ad8229b55cf93d18499b8773
Liam Thys <liam.thys@student.ucll.be>
2018-12-24 14:53:45 +0100

    evaluation change
    app needs restart for changes to be effective

--- 1718_selfAssess_opgave/src/view/panels/EvaluationChangePane.java
+++ 1718_selfAssess_opgave/src/view/panels/EvaluationChangePane.java
@@ -5,15 +5,23 @@ import javafx.beans.value.ObservableStringValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
 
 public class EvaluationChangePane extends GridPane {
     private PropertyStrategy prop;
-    private Button evaluationModeButton;
+    private Button evaluationModeButton,btnOk;
+    private boolean close = false;
 
     public EvaluationChangePane (){
         prop = new PropertyStrategy();
@@ -34,5 +42,23 @@ public class EvaluationChangePane extends GridPane {
         ArrayList<String> content = new ArrayList<>();
         if(property.equals("score") ? content.add("feedback"): content.add("score"));
         prop.save(content);
+
+        Stage dialogStage = new Stage();
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+
+        btnOk = new Button("Ok");
+        VBox vbox;
+        vbox = new VBox(new Text("Restart application neccesary for changes"), btnOk);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setPadding(new Insets(30));
+
+        dialogStage.setScene(new Scene(vbox));
+        dialogStage.show();
+        btnOk.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.exit(0);
+            }
+        });
     }
 }
